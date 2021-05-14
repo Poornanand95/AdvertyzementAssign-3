e20a55b3dd00978bde9283714e1345abc21c5c49
Poornanand Verma <poornanand95@gmail.com>
2021-05-15 00:53:46 +0530

    complete to do

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
-const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
+const input = document.getElementById('input');
+const btn = document.getElementById('add-btn');
+const toDoBox = document.getElementById('to-do-box');
+const dustBin = document.getElementById('delete-here');
+console.log(dustBin);
+var draggables;
 
-draggables.forEach((draggable) => {
-  draggable.addEventListener('dragstart', (e) => {
-    setTimeout(() => (draggable.style.opacity = '0.5'), 0);
-    draggable.classList.add('dragging');
-  });
+// console.log(containers);
+input.addEventListener('input', handleChangeEvent);
 
-  draggable.addEventListener('dragend', (e) => {
-    setTimeout(() => (draggable.style.opacity = '1'), 0);
-    draggable.classList.remove('dragging');
+function handleChangeEvent(e) {
+  inputValue = e.target.value;
+  // console.log(inputValue);
+}
+
+btn.addEventListener('click', handleClickEvent);
+
+function handleClickEvent(e) {
+  if (input.value) {
+    var div = document.createElement('div');
+    div.innerText = input.value;
+    toDoBox.appendChild(div);
+    input.value = '';
+    div.classList.add('draggable');
+    div.setAttribute('draggable', 'true');
+    selectDraggables();
+  }
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener('dragstart', (e) => {
+      setTimeout(() => (draggable.style.opacity = '0.5'), 0);
+      draggable.classList.add('dragging');
+    });
+
+    draggable.addEventListener('dragend', (e) => {
+      setTimeout(() => (draggable.style.opacity = '1'), 0);
+      draggable.classList.remove('dragging');
+      dustBin.classList.remove('dustbin-active');
+    });
   });
-});
+}
+
+function selectDraggables() {
+  draggables = document.querySelectorAll('.draggable');
+  console.log(draggables);
+}
 
 containers.forEach((container) => {
   container.addEventListener('dragover', (e) => {
     e.preventDefault();
     const afterElement = getDragAfterElement(container, e.clientY);
-    const draggable = document.querySelector('.dragging');
+    let draggable = document.querySelector('.dragging');
     function append() {
       if (afterElement == null) {
         container.appendChild(draggable);
@@ -47,3 +80,15 @@ function getDragAfterElement(container, y) {
     { offset: Number.NEGATIVE_INFINITY }
   ).element;
 }
+
+dustBin.addEventListener('dragover', (e) => {
+  let draggable = document.querySelector('.dragging');
+
+  e.preventDefault();
+  function remove() {
+    dustBin.classList.add('dustbin-active');
+    dustBin.appendChild(draggable);
+    dustBin.removeChild(draggable);
+  }
+  remove();
+});
